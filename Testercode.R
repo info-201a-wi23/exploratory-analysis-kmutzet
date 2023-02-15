cc1935b195063b872d35af8e4b69a33fe700eb88
Tammy Nguyen <tammynguyen@Tammys-MacBook-Pro-3.local>
2023-02-14 17:57:46 -0800

    coding changes

--- Testercode.R
+++ Testercode.R
@@ -1,7 +1,51 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("dplyr")
+library("ggplot2")
 
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+bc_data <- read.csv("/Users/tammynguyen/Desktop/Info201/exploratory-analysis-kmutzet/cervicalcancerdata.csv", stringsAsFactor = FALSE)
+
+num_columns <- ncol(bc_data)
+
+#percentage of hormonal contraceptive users?
+hc <- bc_data$Hormonal.Contraceptives
+
+hc_percentage <- paste0(round(sum(hc == 1, na.rm = TRUE) / length(hc) * 100, 2), "%") 
+
+#ratio of those under 30 using contraceptives
+under_thirty <- bc_data %>% group_by(Age, Hormonal.Contraceptives) %>% filter(Age<30)
+under_thirty_ratio <- paste0(round(nrow(under_thirty %>% filter(Hormonal.Contraceptives ==1)) / nrow(under_thirty) *100, 2), "%")
+
+#What is the youngest age of birth control use and what is oldest? 
+youngest_user <- bc_data %>% filter(Hormonal.Contraceptives ==1) %>% filter(Age == min(Age)) %>% pull(Age)
+
+oldest_user <- bc_data %>% filter(Hormonal.Contraceptives ==1) %>% filter(Age == max(Age)) %>% pull(Age)
+
+#How long have they been using it
+youngest_time <- bc_data %>% filter(Hormonal.Contraceptives ==1) %>% filter(Age == min(Age)) %>% pull(Hormonal.Contraceptives..years.)
+
+oldest_time <- bc_data %>% filter(Hormonal.Contraceptives ==1) %>% filter(Age == max(Age)) %>% pull(Hormonal.Contraceptives..years.)
+
+#Add column hormonal Contraceptives and iud together and call it contraceptive
+bc_data <- bc_data %>% mutate(combined_contraceptives = Hormonal.Contraceptives + IUD) 
+
+bc_data <- bc_data %>% mutate(combined_years = Hormonal.Contraceptives..years. + IUD..years.)        
+
+#Plot graph with years of contraceptives (x) and cervical biopsy results
+ggplot(bc_data) + geom_line(aes(x = combined_years, y = Biopsy))
+# or
+ggplot(bc_data) + geom_line(aes(x = Hormonal.Contraceptives..years., y = Biopsy))
+
+#Plot graph with years of iud (x) and cer. cancer biopsy results (y)
+ggplot(bc_data) + geom_line(aes(x = IUD..years., y = Biopsy))
+
+#Age and cervical cancer biopsy results
+ggplot(bc_data) + geom_line(aes(x = Age, y = Biopsy))
+
+#Dot graph of ages using contraceptives 
+ggplot(bc_data) + geom_line(aes(x = Age, y = total_contraceptives))
+
+#
+
+
+
+
 

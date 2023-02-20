59207b8c4662b8167668329961bf492585caaaae
Avallenche <wenyuz8@uw.edu>
2023-02-20 13:57:28 -0800

    charts

--- chart1.R
+++ chart1.R
@@ -4,8 +4,7 @@ library(dplyr)
 bc_data <- read.csv("./cervicalcancerdata.csv", stringsAsFactor = FALSE)
 
 ggplot(bc_data) + 
-  geom_point(aes(x = Hormonal.Contraceptives..years., y = Biopsy)) +
-  labs(title = "Relations of Biopsy and years of getting hormonal contraceptives",
-       x = "Years of hormonal contraceptives",
-       y = "Get Biopsy")
+  geom_histogram(aes(x = Hormonal.Contraceptives..years.)) +
+  labs(title = "Years of getting hormonal contraceptives",
+       x = "Years of hormonal contraceptives")
   

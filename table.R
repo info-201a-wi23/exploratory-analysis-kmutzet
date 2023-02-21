090aed24e074d7b398f8f88ce062b009d5de5143
kmutzet <kmuret@uw.edu>
2023-02-20 20:54:11 -0800

    fixed typo table

--- table.R
+++ table.R
@@ -5,17 +5,18 @@ table$Hormonal.Contraceptives..years. <-
   round(table$Hormonal.Contraceptives..years.,2)
 table$combined_years <- 
   round(table$combined_years,2)
+table$IUD..years. <- round(table$IUD..years.,2)
 table <- table %>% mutate(age_by_decade = round((Age/10-0.5),0))
 table <- table %>%
   group_by(age_by_decade) %>% 
-  summarize(sum(Smokes, na.rm = TRUE), 
+  summarize(sum(Smokes, na.rm = T), 
     sum(combined_contraceptives, na.rm = TRUE), 
     sum(combined_years, na.rm = TRUE),
     sum(Hormonal.Contraceptives, na.rm = TRUE), 
     sum(Hormonal.Contraceptives..years., na.rm = TRUE),
     sum(IUD, na.rm = TRUE),
     sum(IUD..years., na.rm = TRUE),
-    sum(Biopsy), na.rm = TRUE)
+    sum(Biopsy, na.rm = TRUE))
 
 
 

97a6a94d9879aa1c20d7db82239eeea8481c7481
kmutzet <kmuret@uw.edu>
2023-02-20 21:10:50 -0800

    fixed error of table

--- table.R
+++ table.R
@@ -9,20 +9,12 @@ table$IUD..years. <- round(table$IUD..years.,2)
 table <- table %>% mutate(age_by_decade = paste0(round((Age/10-0.5),0)*10,"s"))
 table <- table %>%
   group_by(age_by_decade) %>% 
-  summarize(sum(Smokes, na.rm = T), 
-    sum(combined_contraceptives, na.rm = TRUE), 
-    sum(combined_years, na.rm = TRUE),
-    sum(Hormonal.Contraceptives, na.rm = TRUE), 
-    sum(Hormonal.Contraceptives..years., na.rm = TRUE),
-    sum(IUD, na.rm = TRUE),
-    sum(IUD..years., na.rm = TRUE),
-    sum(Biopsy, na.rm = TRUE))
-#table <- table %>% rename("sum(combined_contraceptives, na.rm = TRUE)" = "combined_contraceptives")
-#table <- table %>% rename("sum(combined_years, na.rm = TRUE)" = "combined_years")
-#table <- table %>% rename("sum(Hormonal.Contraceptives, na.rm = TRUE)" = "hormonal_contraceptives")
-#table <- table %>% rename("sum(Hormonal.Contraceptives..years., na.rm = TRUE)" = "hormonal_contraceptives_years")
-#table <- table %>% rename("sum(IUD, na.rm = TRUE)" = "IUD")
-#table <- table %>% rename("sum(IUD..years., na.rm = TRUE)" = "IUD_years")
-#table <- table %>% rename("sum(Biopsy, na.rm = TRUE)" = "Biopsy")
+  summarize(smokes = sum(Smokes, na.rm = T), 
+    combined_contraceptives = sum(combined_contraceptives, na.rm = TRUE), 
+    combined_years = sum(combined_years, na.rm = TRUE),
+    hormonal_contraceptives = sum(Hormonal.Contraceptives, na.rm = TRUE), 
+    hormonal_contraceptives_years = sum(Hormonal.Contraceptives..years., na.rm = TRUE),
+    iud = sum(IUD, na.rm = TRUE),
+    iud_years = sum(IUD..years., na.rm = TRUE),
+    biopsy = sum(Biopsy, na.rm = TRUE))
 
-#group_by(round((Age/10-0.5),0)) %>%

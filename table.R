a1ac36a34cdc573812c9474aada713559e51c1ec
kmutzet <kmuret@uw.edu>
2023-02-20 21:06:09 -0800

    Fixed names of columns, error with one line

--- table.R
+++ table.R
@@ -6,7 +6,7 @@ table$Hormonal.Contraceptives..years. <-
 table$combined_years <- 
   round(table$combined_years,2)
 table$IUD..years. <- round(table$IUD..years.,2)
-table <- table %>% mutate(age_by_decade = round((Age/10-0.5),0))
+table <- table %>% mutate(age_by_decade = paste0(round((Age/10-0.5),0)*10,"s"))
 table <- table %>%
   group_by(age_by_decade) %>% 
   summarize(sum(Smokes, na.rm = T), 
@@ -17,8 +17,12 @@ table <- table %>%
     sum(IUD, na.rm = TRUE),
     sum(IUD..years., na.rm = TRUE),
     sum(Biopsy, na.rm = TRUE))
-
-
-
+#table <- table %>% rename("sum(combined_contraceptives, na.rm = TRUE)" = "combined_contraceptives")
+#table <- table %>% rename("sum(combined_years, na.rm = TRUE)" = "combined_years")
+#table <- table %>% rename("sum(Hormonal.Contraceptives, na.rm = TRUE)" = "hormonal_contraceptives")
+#table <- table %>% rename("sum(Hormonal.Contraceptives..years., na.rm = TRUE)" = "hormonal_contraceptives_years")
+#table <- table %>% rename("sum(IUD, na.rm = TRUE)" = "IUD")
+#table <- table %>% rename("sum(IUD..years., na.rm = TRUE)" = "IUD_years")
+#table <- table %>% rename("sum(Biopsy, na.rm = TRUE)" = "Biopsy")
 
 #group_by(round((Age/10-0.5),0)) %>%
